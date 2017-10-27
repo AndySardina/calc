1fef3f3488f645484c0c66082e4dd83e5572cdfe
Andy Sardina <andysardina22@gmail.com>
2017-10-27 15:09:59 +0200

    Adding code for summary

--- R/linmodEst.R
+++ R/linmodEst.R
@@ -45,3 +45,27 @@ print.linmod <- function(x, ...)
   print(x$coefficients)
 }
 
+summary.linmod <- function(object, ...)
+{
+  se <- sqrt(diag(object$vcov))
+  tval <- coef(object) / se
+  TAB <- cbind(Estimate = coef(object),
+               StdErr = se,
+               t.value = tval,
+               p.value = 2*pt(-abs(tval), df=object$df))
+  res <- list(call=object$call,
+              coefficients=TAB)
+
+  class(res) <- "summary.linmod"
+  res
+}
+
+
+print.summary.linmod <- function(x, ...)
+{
+  cat("Call:\n")
+  print(x$call)
+  cat("\n")
+  printCoefmat(x$coefficients, P.value=TRUE, has.Pvalue=TRUE)
+}
+
